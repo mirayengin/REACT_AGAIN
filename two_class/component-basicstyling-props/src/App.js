1f962c604eea337187f68664ada7e1947f8458d5
mirayengin <erhanserkansabriye@gmail.com>
2024-02-01 01:30:27 +0300

    basit css yapıldı

--- two_class/component-basicstyling-props/src/App.js
+++ two_class/component-basicstyling-props/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
 
+
 function App() {
   return (
     <div className="App">
