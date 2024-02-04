38fd7a5f21312f9778b20f5e163e6dc415a4939e
mirayengin <erhanserkansabriye@gmail.com>
2024-02-04 12:00:31 +0300

    probs dersi başlıyor

---
+++ three_class/rc02_props/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import data from "./data";
+
+function App() {
+  
+
+      {/* Msg componeneti aslında bir fonksiyon olduğundan böyle de yazılabilir.  */}
+   
+
+      {/* <Person name={data[0].name} img={data[0].img} tel={data[0].tel} />
+      <Person name={data[1].name} img={data[1].img} tel={data[1].tel} />
+      <Person name={data[2].name} img={data[2].img} tel={data[2].tel} /> */}
+
+
+}
+
+export default App;
