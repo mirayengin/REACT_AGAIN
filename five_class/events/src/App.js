94ea3404fed00393c39559df1f0fc3533623d006
mirayengin <erhanserkansabriye@gmail.com>
2024-02-14 18:16:52 +0300

    useStateHook anlatımına başlandı

--- five_class/events/src/App.js
+++ five_class/events/src/App.js
@@ -1,10 +1,13 @@
 import Events from "./component/1-events/Events";
+import Counter from "./component/2-class_component/Counter";
 
 
 function App() {
   return (
     <div className="App">
       <Events />
+      <Counter />
+      <Counter count={5} />
     </div>
   );
 }
--- five_class/events/src/component/2-class_component/Counter.jsx
+++ five_class/events/src/component/2-class_component/Counter.jsx
@@ -1,9 +1,44 @@
 
+import { Component } from 'react'
 
-const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+class Counter extends Component {
+  //! rconst ile bu constructer cıkıyor
+  constructor(props) {
+    super(props)
+  
+    //! count state ine başlangıç değeri atatık.
+    this.state = {
+      count: props.count || 0,
+    }
+    //? increment metodunun counter clasına bağlanması (bind)
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    this.setState({
+      count : this.state.count + 1
+    })
+  }
+
+  //! Arrow func. yaptığımızda otomatik binding yapmış oluyoruz.
+  decrement =() => {
+    this.setState({
+      count : this.state.count - 1
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="container text-center mt-4">
+        <h1>CLASS ALL COMPONENT</h1>
+        <h2 className='display-4 text-danger'>Count :{this.state.count} </h2>
+      <button onClick={this.increment}  className="btn btn-success">INC</button>
+      
+      <button onClick={this.decrement} className="btn btn-danger">DEC</button>
+      </div>
+    )
+  }
 }
 
 export default Counter
