05d0641075772ce0d43cdc22591d669521bf0b52
mirayengin <erhanserkansabriye@gmail.com>
2024-02-20 01:15:05 +0300

    formlarda event olaylarını ve elementlerin propertylerini anlatıyor 1. ders bitti

--- six_class/rc06_forms-otherEvents_bootstrap/src/components/1-forms/Formobject.jsx
+++ six_class/rc06_forms-otherEvents_bootstrap/src/components/1-forms/Formobject.jsx
@@ -2,49 +2,85 @@ import { useState } from "react";
 
 const Formobject = () => {
 
-  return (
-    <form >
-      <h1 className="display-5 text-danger">FORM OBJECT</h1>
+  const [formValues, setFormValues] = useState({
+    username: "",
+    email: "",
+    password : ""
+  })
 
-      <div className="mb-3">
-        <label htmlFor="username" className="form-label">
-          Username: <span className="text-danger"></span>
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="username"
- 
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Email : <span className="text-danger"></span>
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
+  const { username, email, password } = formValues; //! destr
 
-        />
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    alert(`
+      username: ${username},
+      email: ${email},
+      password: ${password}
+    `)
+
+  }
+
+  const handleForm = (e) => {
+    console.log(e.target)
+  }
+
+
+
+
+
+
+
+  return (    
+    <form onSubmit={handleSubmit}>
+      <h1 className="display-5 text-danger">FORMS OBJECT 1.DERS BİTTİ</h1>
+  <div className="mb-3">
+    <label htmlFor="username" className="form-label">
+      Username : <span>{username}</span> 
+    </label>
+    <input
+      type="text"
+      className="form-control"
+      id="username"
+      value={username} 
+      onChange={handleForm}
+    />
+
       </div>
+      
       <div className="mb-3">
-        <label htmlFor="password" className="form-label">
-          Password : <span className="text-danger"></span>
-        </label>
-        <input
-          type="password"
-          className="form-control"
+    <label htmlFor="email" className="form-label">
+      Email : <span> {email} </span>
+    </label>
+    <input
+      type="email"
+      className="form-control"
+      id="email"
+      value={email} 
+      onChange={handleForm}
+    />
+
+      </div>
+      
+  <div className="mb-3">
+    <label htmlFor="password" className="form-label">
+          Password
+    </label>
+    <input
+      type="password"
+      className="form-control"
           id="password"
-   
-        />
-      </div>
+          value={password}
+          onChange={handleForm}
+    />
+  </div>
 
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
-  );
-};
+  <button type="submit" className="btn btn-primary">
+    Submit
+  </button>
+</form>
+   
+  )
+}
 
 export default Formobject;
