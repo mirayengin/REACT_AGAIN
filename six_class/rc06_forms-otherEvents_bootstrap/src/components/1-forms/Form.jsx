18176a9e2624af09ee6e71e4ee44f542ff449c1a
mirayengin <erhanserkansabriye@gmail.com>
2024-02-19 13:28:39 +0300

    form da submit yapıldı

--- six_class/rc06_forms-otherEvents_bootstrap/src/components/1-forms/Form.jsx
+++ six_class/rc06_forms-otherEvents_bootstrap/src/components/1-forms/Form.jsx
@@ -1,7 +1,80 @@
+import { useState } from "react";
 
 const Form = () => {
-  return (
-    <div>Form</div>
+  const [username, setUsername] = useState("Aslı");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    alert(`
+      username: ${username},
+      email: ${email},
+      password: ${password}
+    `)
+  }
+
+  const handleUserChange = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  }
+
+
+
+
+
+  return (    
+    <form onSubmit={handleSubmit}>
+      <h1 className="display-5 text-danger">FORMS</h1>
+  <div className="mb-3">
+    <label htmlFor="username" className="form-label">
+      Username : <span>{username}</span> 
+    </label>
+    <input
+      type="text"
+      className="form-control"
+      id="username"
+      //! value property si ilk değer verilmesi durumunda sayfa acçılırken göstermek için önemli ğer bunu yazmazsak bu inputta görülmezdi verdiğimiz ilk değer
+      value={username} 
+      onChange={handleUserChange}
+    />
+
+      </div>
+      
+      <div className="mb-3">
+    <label htmlFor="email" className="form-label">
+      Email : <span> {email} </span>
+    </label>
+    <input
+      type="email"
+      className="form-control"
+      id="email"
+      value={email} 
+      onChange={(e) => setEmail(e.target.value)}
+    />
+
+      </div>
+      
+  <div className="mb-3">
+    <label htmlFor="password" className="form-label">
+          Password
+    </label>
+    <input
+      type="password"
+      className="form-control"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+    />
+  </div>
+
+  <button type="submit" className="btn btn-primary">
+    Submit
+  </button>
+</form>
+   
   )
 }
 
