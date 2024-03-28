ca8de088ee3b12c65ca0ef45fd2a67942f5598ea
mirayengin <erhanserkansabriye@gmail.com>
2024-03-28 15:35:23 +0300

    MouseEvent lar anlatıldı.

--- six_class/rc06_forms-otherEvents_bootstrap/src/components/3-mouseEvents/MouseEvent.jsx
+++ six_class/rc06_forms-otherEvents_bootstrap/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
 
 const MouseEvent = () => {
+    const [showHint, setshowHint] = useState(false);
+    const [toogle, settoogle] = useState(false)
+
+    const [coordX, setCoordX] = useState("");
+    const [coordY, setCoordY] = useState("")
+
+    const handleDoubleClick = (e) => {
+        settoogle(!toogle)
+
+        toogle
+            ? (e.target.className = "bg-success text-light w-50 p-4 mt-4")
+            : (e.target.className = "bg-danger text-light w-50 p-4 mt-4");
+    };
+
+
+    const handleMouseMove = (e) => { 
+        setCoordX(e.nativeEvent.offsetX); //? bu nativeEvent ile elementi noktasal olarak event liyoruz.
+        setCoordY(e.nativeEvent.offsetY);
+        // setCoordX(e.pageX); //! bu sayfa içindeki noktayı veriyor.
+        // setCoordY(e.pageY);
+    };
 
 
   return (
@@ -10,23 +31,30 @@ const MouseEvent = () => {
           <div
               id="todo-1"
               className="bg-success text-light w-50 p-4"
-
+              onMouseOut={() => setshowHint(false)}  
+              onMouseEnter ={() => setshowHint(true)}
           >
               todo item 1 - onMouseEnter/onMouseOut
           </div>
+          {showHint && <p>Hello React</p>}
+          {/* //? Burada biz tru veya false olmasına göre bu p elementini DOM a basılmasını sağladık. Şart ve ya koşul olduğu için hemen jsx alanına süslü açtık ve bu koşulun çalışmasını sağladık. */}
         
 
           <div
               id="todo-2"
               className="bg-success text-light w-50 p-4 mt-4"
-
+              onDoubleClick = {handleDoubleClick}
           >
               todo item 2 - onDoubleClick
           </div>
 
+
+
+
           <div
               id="todo-3"
               className="bg-success text-light w-50 p-4 my-4"
+              onMouseMove = {handleMouseMove}
 
           >
               todo item 3 - onMouseMove
@@ -34,7 +62,7 @@ const MouseEvent = () => {
 
           <p>X and Y</p>
           <p className="text-danger fw-bold">
-
+            {coordX}    {coordY}
           </p>
       </div>
   );
