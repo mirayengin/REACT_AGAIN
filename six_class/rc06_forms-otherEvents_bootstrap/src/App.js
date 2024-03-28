6aca3a65b493d8e3b740e61e57b0e0e4d7d49060
mirayengin <erhanserkansabriye@gmail.com>
2024-03-28 14:58:40 +0300

    keyboardclipboard anlatıldı.

--- six_class/rc06_forms-otherEvents_bootstrap/src/App.js
+++ six_class/rc06_forms-otherEvents_bootstrap/src/App.js
@@ -1,14 +1,14 @@
 // import Form from "./components/1-forms/Form";
-import Formobject from "./components/1-forms/Formobject";
-// import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
+// import Formobject from "./components/1-forms/Formobject";
+import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
 // import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      <Formobject />
-      {/* <KeyboardEvents /> */}
+      {/* <Formobject /> */}
+      <KeyboardEvents />
       {/* <MouseEvent /> */}
     </div>
   );
--- six_class/rc06_forms-otherEvents_bootstrap/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ six_class/rc06_forms-otherEvents_bootstrap/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,23 +1,58 @@
 import { useState } from "react";
 
 const KeyboardClipboard = () => {
+  const [inputValue, setinputValue] = useState("");
+  // const [keyDown, setKeyDown] = useState("");
+
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode)
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert("Please dont enter a number")
+      e.preventDefault() //! bunu kullanmasaydık otomatik react uyarı versede rakamı DOM a yazıyordu fakat bununla biz bu davranışını kontrol ediyoruz yani engelliyoruz.
+    }
+
+    e.keyCode === 13 && setinputValue("")
+    
+
+  }
+
+  const handleAreaPaste = (e) => {
+    e.target.style.border ="2px solid red" 
+    console.log(e.target.value)
+    e.target.value = e.clipboardData.getData("text") //? kopyala yapıştır olayında clipboard da bulunan verileri okumak için kullanılır. BU olayı browser ve bizim projemizde kullanıyor.
+    e.preventDefault() //? Browser ın kopyala ypıştırını engellemek için bunu kullanıyoruz yoksaki 2 kere çalışıyor.
+    e.target.value += e.clipboardData.getData("text").toLocalLowerCase() //? bunun la her gelen veri eklenir öncekine ve küçük harfle olur.
+  }
 
   return (
     <div className="container text-center">
       <h1>CLIPBOARD EVENTS</h1>
       <input
         className="form-control"
+        value={inputValue} //? bu olmaz ise değeri kontrol edemeyiz.(sıfırlama vs)
         type="text"
+        // onChange={(e) => (inputValue = e.target.value)} //!tek işlem için parantez çoklu için süslü açıyoruz aşağıdaki gibi yazabiliriz.
+        // onChange={(e) => {
+        //   inputValue = e.target.value
+        //   console.log(inputValue)
+        // }}
+        // onChange={(e) => setinputValue((e.target.value).toLocaleUpperCase())} //? bunun ile input yazılan p ye büyük harf ile yazılıyor.
+        onChange={(e) => { //! bunun ile yazılan her şey input ve p de büyük harf olarak yazılıyor.
 
+          e.target.value = e.target.value.toLocaleUpperCase()
+          setinputValue(e.target.value)
+        }}
+        onKeyDown={handleKeyDown} //? bir rakama basıldığında aski larşılığını almak için bu özeeliği kullandık.
       />
+
       <p
         onCopy={(e) => {
-          e.preventDefault();
+          e.preventDefault(); //? burada copy işlemini p elementinde engelledik.
           alert("you cant copy document");
         }}
         className="text-start mt-4"
       >
-    
+        {inputValue}
       </p>
 
       <textarea
@@ -26,6 +61,7 @@ const KeyboardClipboard = () => {
         id="area"
         cols="30"
         rows="10"
+        onPaste={handleAreaPaste}
 
       ></textarea>
     </div>
