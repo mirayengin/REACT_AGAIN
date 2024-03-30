ada668da17d30e231d3414daee720fa39f5067d7
mirayengin <erhanserkansabriye@gmail.com>
2024-03-31 02:47:21 +0300

    language card projesi iskeleti kurldu.

--- languageCard/src/App.js
+++ languageCard/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import Card from "./components/Card";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <p>React Learn</p>
-    </div>
+    <>
+      <Header />
+      <Card/>
+    </>
   );
 }
 
