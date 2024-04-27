9aaadd08b6522dbfb547e46a72a7e718e3e9856b
mirayengin <erhanserkansabriye@gmail.com>
2024-04-27 13:34:17 +0300

    card yapısı düzelttildi

--- languageCard/src/components/Card.jsx
+++ languageCard/src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = () => {
   return (
     <Container  className="rounded-4 mt-4 p-2" style = {{background : "#f48b29"}}>
       <h1 className="text-white my-2" >Languages</h1>
-      <Row>
+      <Row className="g-3 justify-content-center">
         {data.map((lang, index) => {
           return (
             <Col sm={6} md={4} lg={3} key={index} >
--- languageCard/src/components/Language.jsx
+++ languageCard/src/components/Language.jsx
@@ -6,9 +6,11 @@ import Image from "react-bootstrap/Image";
   const Language = ({name,img,options}) => {
   // console.log(lang);
   return (
-    <Container>
+    <Container className="p-4 rounded-2" style= {{backgroundColor : "peachpuff"}}>
       <Container>
         <Image src={img} width="60%"></Image>
+        <h3 className="display-6">{name}</h3>
+        <h4>İkinci ders bitti</h4>
       </Container>
     </Container>
   )
